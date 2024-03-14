7d383672ea0c3b6f7a583d97f8a3a955422c6439
krishna503 <ghkrishna503@gmail.com>
2024-03-14 21:01:15 +0530

    Day2

--- app.js
+++ app.js
@@ -1,106 +1,98 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM  from 'react-dom';
 
-//JSX
-// Javascript element
+const Heading =  <img className='logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-WWyr1Bo0GrohgOzpbLeAGHcysfLFYgAXEg&usqp=CAU'></img>
 
-const Heading = () =>   <img className="img" alt="logo" src="https://foodvilla.no/src/img/logo.png"></img>
-
-const Head =() => (
-<div className="Head">
-<Heading />
-<div className="Navitem">
-    <ul>
+const Head = ()=>{
+    return (
+    <div className='head'>
+        {Heading}
+      
+        <div className='list'>
+        <ul>
         <li>Home</li>
+        <li>Contact</li>
         <li>About</li>
-        <li>Cart</li>
-    </ul>
-</div>
-</div>
-)
-
-const Restaulist = 
-[
-    {
-    type: "Restaurent",
-   data:{img: "https://www.shutterstock.com/image-illustration/situbondo-indonesia-june-06-2023-260nw-2313601567.jpg",
-     Name:"KFC",
-     Cuisines:["Wings","lollipop"],
-     Price:"250 for two",
-     Rating: "4.5 Stars"}
-    } ,
-    {
-     type:"Restaurent",
-     data:
-  {img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiHSySjzdkopvz3wyUDQKlq8DtANLLPN3wZA&usqp=CAU",
-     Name:"Burger King",
-     Cuisines:["Wings","lollipop"],
-     Price:"250 for two",
-     Rating: "4.5 Stars"}
-    },
-    {
-     type:"Restaurent",
-     data:
-  {img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc-xy12M7uJSewVYTP1omsMyjTnteQJno_4Q&usqp=CAU",
-     Name:"Paradise",
-     Cuisines:["Wings","lollipop"],
-     Price:"250 for two",
-     Rating: "4.5 Stars"}
-    },
-    {
-     type:"Restaurent",
-  data:{img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsUeBr3mRNTzWlA_rDMx8ZjXJxEcWKsMzdQw&usqp=CAU",
-     Name:"Meghana Foods",
-     Cuisines:["Wings"," lollipop"],
-     Price:"250 for two",
-     Rating: "4.5 Stars"}
-    }
-]
-
-const Resturentcard = ({img,Name,Cuisines,Price,Rating}) =>{
-    return(
-    <div className="card">
-    <img className="image" alt="kfc" src={img}/>
-    <h3>{Name}</h3>
-    <h3>{Cuisines}</h3>
-    <h3>{Price}</h3>
-    <h3>{Rating}</h3>
-  </div>
-)}
-const Body =() =>
-{
-   
-return(
-
-    <div className="Restaurent-list">
-        {
-            Restaulist.map((Restaurent)=>
-            {
-                return    <Resturentcard {...Restaurent.data}/>
-            }
-            )
-        }
+        <li>Help</li>
+        </ul>
+        </div>
+       
     </div>
+    )
+}
+ const Rastaulist =  [
+        {
+    type:"Restaurent",
+    data:{
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIjQxRreOUVNjulD5dsi_BKprXOUYAj-buoA&usqp=CAU",
+        Name:"KFC",
+        Cusines:["wings","popcorn"],
+        Address:"188/9 Amruthakani Nilya,somweswara layout,billekhalli"
+    }
+        },
+        {
+        type:"Restaurent",
+        data:{
+            img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt5VWpkR62aW-lOZAxFsUuKG6x5cTRQs9KxA&usqp=CAU",
+            Name:"Burger King",
+            Cusines:["Drinks","Burger"],
+            Address:"188/9 Amruthakani Nilya,somweswara layout,billekhalli"
+        }
+    }, {
+        type:"Restaurent",
+        data:{
+            img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt5VWpkR62aW-lOZAxFsUuKG6x5cTRQs9KxA&usqp=CAU",
+            Name:"Burger King",
+            Cusines:["Drinks","Burger"],
+            Address:"188/9 Amruthakani Nilya,somweswara layout,billekhalli"
+        }
+    }, {
+        type:"Restaurent",
+        data:{
+            img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt5VWpkR62aW-lOZAxFsUuKG6x5cTRQs9KxA&usqp=CAU",
+            Name:"Burger King",
+            Cusines:["Drinks","Burger"],
+            Address:"188/9 Amruthakani Nilya,somweswara layout,billekhalli"
+        }
+    }
 
+] 
+ 
+const Cards =({img,Name,Cusines,Address})=>{
+    return(
+    <div className="Nav-item">
+    <img className='Restimg'src={img} />
+    <h2>{Name}</h2>
+    <h3>{Cusines}</h3>
+    <h3>{Address}</h3>
+    </div>
+    )
+}
+ 
+const Body = () =>{
+return(
+    <div className="map">
+        {
+    Rastaulist.map((Restaurent) =>
+    {
+       return <Cards {...Restaurent.data} />
+    }
+      )}
+    </div>
 )
 }
+const Footer = () =>{
+    <h2>Footer</h2>
+}
 
-const Footer =() =>
-(
-<h2>footer</h2>
-)
-// Function or Component
-const Applayout = () => (
+const Applayout = ()=>{
+    return (
     <>
     <Head />
     <Body />
     <Footer />
     </>
-)
-
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<Applayout />);
-
+    )
+}
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Applayout/>)
